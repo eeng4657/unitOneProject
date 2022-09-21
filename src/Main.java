a2aae057b4fa05923983e2fb2118056e0df74668
eeng4657 <eeng4657@bths.edu>
2022-09-21 12:11:16 -0400

    Added all commands to ask user for bill cost, tip percentage, and number of people, and to store those variables.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
         Scanner b = new Scanner (System.in);
         System.out.println("What is the cost of the bill? ");
         double bill = b.nextDouble();
-        System.out.print(bill);
+        Scanner t = new Scanner (System.in);
+        System.out.println("What is your tip percentage? ");
+        int tipPercent = t.nextInt();
+        Scanner p = new Scanner (System.in);
+        System.out.println("How many people are in your party? ");
+        int peopleNum = p.nextInt();
     }
 }
